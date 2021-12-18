b9fc66ddeeb11cfb25c910796d9ae59932b8aae5
AnaVomvylas <anavomv@gmail.com>
2021-12-18 22:12:11 +0100

    Format

--- src/secret_santa.py
+++ src/secret_santa.py
@@ -1,6 +1,7 @@
 import random
 
-emails = ['1','2','3']
+emails = ["1", "2", "3"]
+
 
 def execute_secret_santa(senders: list) -> dict:
     remaining_receivers = senders.copy()
@@ -9,12 +10,13 @@ def execute_secret_santa(senders: list) -> dict:
     for sender in senders:
         if remaining_receivers == [sender]:
             return {}
-        receiver = get_random_receiver(sender,remaining_receivers)
+        receiver = get_random_receiver(sender, remaining_receivers)
         remaining_receivers.remove(receiver)
         pairs[sender] = receiver
     return pairs
 
-def get_random_receiver(sender:str ,receivers:list) -> str:
+
+def get_random_receiver(sender: str, receivers: list) -> str:
     receivers_excluding_sender = list.copy(receivers)
     if sender in receivers_excluding_sender:
         receivers_excluding_sender.remove(sender)
@@ -25,5 +27,3 @@ if __name__ == "__main__":
     pairs = {}
     while len(pairs) == 0:
         pairs = execute_secret_santa()
-    
-    
